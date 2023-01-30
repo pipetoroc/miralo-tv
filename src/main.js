e600565a16f0251f8e393ae7cb8471f367f2d685
Felipe Toro Castilla <ftorocastilla@gmail.com>
2023-01-30 13:42:37 -0300

    feat: trending section with scroll horizontal

--- src/main.js
+++ src/main.js
@@ -1,31 +1,47 @@
 const API_KEY = '05b80c31b8ddf6ca35cb30a17cc64420';
-const URL = `https://api.themoviedb.org/3/movie/76341?api_key=${API_KEY}`;
 
-async function getMovies(API_KEY) {
-  const trend = `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`;
+async function getRecomendation() {
+  const trend = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
 
   const response = await fetch(trend);
   const data = await response.json();
 
   const { results } = data;
 
-  console.log(results);
-
   results.map((item) => {
     const {
       title, name, overview, poster_path,
     } = item;
+    const recomendationSection = document.getElementById('recomendations');
 
-    if (title) {
-      const h2 = document.createElement('h2');
-      h2.innerHTML = title;
-      document.body.appendChild(h2);
-    } else {
-      const h2 = document.createElement('h2');
-      h2.innerHTML = name;
-      document.body.appendChild(h2);
-    }
+    const article = document.createElement('article');
+    article.className = 'recomendations__article';
+
+    // if (title) {
+    //   const h2 = document.createElement('h2');
+    //   h2.innerHTML = title;
+    //   h2.className = 'main__h2';
+    //   article.appendChild(h2);
+    // } else {
+    //   const h2 = document.createElement('h2');
+    //   h2.innerHTML = name;
+    //   h2.className = 'main__h2';
+    //   article.appendChild(h2);
+    // }
+
+    const poster = document.createElement('img');
+    poster.src = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+    poster.className = 'main__img';
+    poster.setAttribute('alt', `Imagen de poster ${title}`);
+    article.appendChild(poster);
+
+    const description = document.createElement('p');
+    description.innerHTML = overview;
+    description.className = 'main__overview';
+    article.appendChild(description);
+
+    recomendationSection.appendChild(article);
   });
 }
 
-getMovies(API_KEY);
+getRecomendation(API_KEY);
