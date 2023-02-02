020e04da5d78addceb47f37b9d40edad5ff51aec
Felipe Toro Castilla <ftorocastilla@gmail.com>
2023-02-02 12:30:59 -0300

    feat: add toggle in text description for each movie

--- src/main.js
+++ src/main.js
@@ -8,36 +8,32 @@ async function getRecomendation() {
   const data = await response.json();
 
   const { results } = data;
+  console.log(results);
 
-  results.map((item) => {
-    const {
-      title, name, overview, poster_path,
-    } = item;
+  let count = 1;
+  results.forEach((item) => {
+    const { title, name, overview, poster_path } = item;
 
     const article = document.createElement('article');
     article.className = 'recomendations__article';
-
-    // if (title) {
-    //   const h2 = document.createElement('h2');
-    //   h2.innerHTML = title;
-    //   h2.className = 'main__h2';
-    //   article.appendChild(h2);
-    // } else {
-    //   const h2 = document.createElement('h2');
-    //   h2.innerHTML = name;
-    //   h2.className = 'main__h2';
-    //   article.appendChild(h2);
-    // }
 
     const poster = document.createElement('img');
     poster.src = `https://image.tmdb.org/t/p/w500/${poster_path}`;
     poster.className = 'main__img';
     poster.setAttribute('alt', `Imagen de poster ${title}`);
+    poster.setAttribute('loading', 'lazy');
     article.appendChild(poster);
 
     const description = document.createElement('p');
     description.innerHTML = overview;
     description.className = 'main__overview';
+    description.id = `description${count++}`;
+
+    description.addEventListener('click', (e) => {
+      const parrafo = document.getElementById(e.target.id);
+      parrafo.classList.toggle('noShow');
+    });
+
     article.appendChild(description);
 
     recomendationSection.appendChild(article);
@@ -50,14 +46,12 @@ async function getCategories() {
   const container = document.createElement('div');
   container.className = 'main__container';
 
-
   const response = await fetch(URL_CATEGORY);
   const data = await response.json();
-  console.log(data);
 
   const { genres } = data;
 
-  genres.map((category) =>{
+  genres.map((category) => {
     const button = document.createElement('button');
     button.textContent = category.name;
     button.className = 'main__button';
